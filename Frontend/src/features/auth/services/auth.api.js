03f1e3a728077dde04ab53bd079e4235b922e169
Parth Chauhan <parthchauhan25125@gmail.com>
2026-06-26 12:09:22 +0530

    feat: add axios for API calls and implement authentication functions

---
+++ Frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { BACKEND_URI } from "../../../const";
+
+const api=axios.create({
+    baseURL:BACKEND_URI,
+    withCredentials:true
+})
+
+export async function register({ username, email, password }) {
+    try{
+    const response=await api.post("/auth/register", { username, email, password },)
+
+    return response.data;
+
+    }catch(err){
+        console.log(err);
+    }
+}
+
+export async function login({email,password}){
+    try{
+        const response=await api.post("auth/login",{email,password});
+        return response.data;
+    }catch(err){
+        console.log(err)
+    }
+}
+
+export async function logout(){
+    try{
+        const response=await api.get("/auth/logout");
+        return response.data;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+export async function getMe(){
+    try{
+        const response=await api.get("/auth/get-me");
+
+        return response.data;
+    }catch(err){
+        console.log(err)
+    }
+}
